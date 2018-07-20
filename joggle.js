8cf7bcd75bac6574499d8be5b919605126c78634
Ian Hovander <ianhovander@gmail.com>
2018-07-19 21:37:27 -0700

    Shake testing

--- joggle.js
+++ joggle.js
@@ -12,6 +12,21 @@ $( document ).ready(
     }
     // Initial resize
     resizeReaction();
+	
+	//listen to shake event
+	var shakeEvent = new Shake({threshold: 5});
+	shakeEvent.start();
+	window.addEventListener('shake', function(){
+		alert("Shaked");
+	}, false);
+
+	//stop listening
+	function stopShake(){
+		shakeEvent.stop();
+	}
+
+	//check if shake is supported or not.
+	if(!("ondevicemotion" in window)){alert("Not Supported");}
   }
 );
 
